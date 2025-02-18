c10caae9cf0846256a714170dbd043e45f848c19
AwasthiNikhil <nikhilawasthi944@gmail.com>
2025-02-18 13:52:26 +0545

    register and logni

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,19 +5,51 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    function register(Request $request) {
+    function register(Request $request)
+    {
         $validated = $request->validate([
-            'name'=>'required',
-            'email'=>'required|email',
-            'password'=>'required|confirmed|min:3'
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:3'
         ]);
+
+        $validated['password'] = Hash::make($validated['password']);
 
         $user = User::create($validated);
 
-        echo "<pre>";
-        print_r($user);
+        $token = $user->createToken('auth_token')->accessToken;
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user,
+            'message' => 'User created successfully.'
+        ]);
+    }
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:3'
+        ]);
+
+        if (Auth::attempt(['email' => $validated['email'], 'password' => $validated['password']])) {
+            $user = Auth::user();
+
+            $token = $user->createToken('auth_token')->accessToken;
+
+            return response()->json([
+                'token' => $token,
+                'user' => $user,
+                'message' => 'User logged in successfully.'
+            ]);
+        }
+        return response()->json([
+            'message' => 'Invalid credss.'
+        ]);
     }
 }
